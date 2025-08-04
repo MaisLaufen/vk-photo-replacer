07a6fa6d5c8bdc0550b20c3797c620e0f6fad060
MaisLaufen <vip.eftifanov@mail.ru>
2025-08-05 02:35:23 +0700

    [Add] Token status in popup menu

--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,23 @@
-document.getElementById("auth").addEventListener("click", () => {
-  const clientId = "6287487";
-  const redirectUri = "https://oauth.vk.com/blank.html"; 
-  const scope = "photos";
-  const authUrl = `https://oauth.vk.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=token`;
+document.addEventListener("DOMContentLoaded", () => {
+  const authBtn = document.getElementById("auth");
+  const statusText = document.getElementById("status");
 
-  chrome.tabs.create({ url: authUrl });
+  chrome.storage.local.get(["vkToken"], (result) => {
+    if (result.vkToken) {
+      statusText.textContent = "Token getted";
+      statusText.style.color = "green";
+    } else {
+      statusText.textContent = "No token";
+      statusText.style.color = "red";
+    }
+  });
+
+  authBtn.addEventListener("click", () => {
+    const clientId = "6287487";
+    const redirectUri = "https://oauth.vk.com/blank.html";
+    const scope = "photos";
+    const authUrl = `https://oauth.vk.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=token`;
+
+    chrome.tabs.create({ url: authUrl });
+  });
 });
