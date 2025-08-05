df1f35ed1288f0df8009223ddcc9d006d3457041
MaisLaufen <vip.eftifanov@mail.ru>
2025-08-05 23:34:50 +0700

    [Add] Deleting token from browser storage

--- popup/popup.js
+++ popup/popup.js
@@ -1,17 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const authBtn = document.getElementById("auth");
+  const clearBtn = document.getElementById("clear");
   const statusText = document.getElementById("status");
 
+  // Проверка токена при открытии popup
   chrome.storage.local.get(["vkToken"], (result) => {
     if (result.vkToken) {
-      statusText.textContent = "Token getted";
-      statusText.style.color = "green";
+      statusText.textContent = "Token saved";
     } else {
       statusText.textContent = "No token";
-      statusText.style.color = "red";
     }
   });
 
+  // Авторизация
   authBtn.addEventListener("click", () => {
     const clientId = "6287487";
     const redirectUri = "https://oauth.vk.com/blank.html";
@@ -19,5 +20,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const authUrl = `https://oauth.vk.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=token`;
 
     chrome.tabs.create({ url: authUrl });
+  });
+
+  // Очистка токена
+  clearBtn.addEventListener("click", () => {
+    chrome.storage.local.remove(["vkToken"], () => {
+      statusText.textContent = "Token cleared";
+    });
   });
 });
